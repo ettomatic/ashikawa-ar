30ff39e1f2268a81178c470a29f524353a8f98e2
moonglum <moonglum@moonbeamlabs.com>
2012-09-27 14:28:57 +0200

    Restructured Query Spec

--- spec/integration/query_spec.rb
+++ spec/integration/query_spec.rb
@@ -1,43 +1,47 @@
-  describe "querying for documents" do
-    before(:all) {
-      Ashikawa::AR.setup :default, ARANGO_HOST
-      database = Ashikawa::Core::Database.new ARANGO_HOST
-      @collection = database["people"]
-      @collection.truncate!
-      @johnny = @collection.create name: "Johnny"
-      @jens = @collection.create name: "Jens"
-    }
+require 'integration/spec_helper'
 
-    subject { Person }
+describe "querying for documents" do
+  before(:all) {
+    require 'examples/person.rb'
 
-    it "should find a document by ID" do
-      person = Person.find @johnny.id
-      person.should be_instance_of Person
-      person.name.should == "Johnny"
-    end
+    Ashikawa::AR.setup :default, ARANGO_HOST
+    database = Ashikawa::Core::Database.new ARANGO_HOST
+    @collection = database["people"]
+    @collection.truncate!
+    @johnny = @collection.create name: "Johnny"
+    @jens = @collection.create name: "Jens"
+  }
 
-    it "should find documents by aql query" do
-      people = Person.find_by_aql "FOR u IN people RETURN u"
-      people.length.should == @collection.length
-      people.first.should be_instance_of Person
-    end
+  subject { Person }
 
-    it "should find documents by example" do
-      people = Person.by_example name: "Johnny"
-      people.length.should == 1
-      people.first.should be_instance_of Person
-      people.first.name.should == "Johnny"
-    end
-
-    it "should find one document by example" do
-      person = Person.first_example name: "Johnny"
-      person.should be_instance_of Person
-      person.name.should == "Johnny"
-    end
-
-    it "should return all documents" do
-      people = Person.all
-      people.length.should == @collection.length
-      people.first.should be_instance_of Person
-    end
+  it "should find a document by ID" do
+    person = subject.find @johnny.id
+    person.should be_instance_of subject
+    person.name.should == "Johnny"
   end
+
+  it "should find documents by aql query" do
+    people = subject.find_by_aql "FOR u IN people RETURN u"
+    people.length.should == @collection.length
+    people.first.should be_instance_of subject
+  end
+
+  it "should find documents by example" do
+    people = subject.by_example name: "Johnny"
+    people.length.should == 1
+    people.first.should be_instance_of subject
+    people.first.name.should == "Johnny"
+  end
+
+  it "should find one document by example" do
+    person = subject.first_example name: "Johnny"
+    person.should be_instance_of subject
+    person.name.should == "Johnny"
+  end
+
+  it "should return all documents" do
+    people = subject.all
+    people.length.should == @collection.length
+    people.first.should be_instance_of subject
+  end
+end
